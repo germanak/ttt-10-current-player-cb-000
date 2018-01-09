4d28fe4eb4079835e1111883cfb0f608100a01a3
Alec German <alec_german@yahoo.com>
2018-01-09 00:26:34 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -2,8 +2,8 @@ def turn_count(board)
   turns = 0
 
   board.each do |position|
-    if board[position] == "X" || board[position] == "O"
-      turn += 1
+    if position == "X" || position == "O"
+      turns += 1
     end
   end
 
